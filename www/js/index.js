c81cc74fde44287de327694a9096270ff2bfcab7
ndevich <devichnadine@gmail.com>
2014-05-17 21:41:01 -0500

    bombz

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,4 @@
 
-function animate()
-{
-	var div = document.getElementById("div1");
-	
-	var r = div.getAttribute("r");
-	if (r == "" || typeof(r) == "undefined" || r == null)
-	{
-		div.setAttribute("r", "1");
-		div.setAttribute("radius", "200");
-		r = 1;
-	}
-	else
-		r = +r;
-		
-	var radius = div.getAttribute("radius");
-	radius = +radius;
-	
-	if (r == 1)
-	{
-		radius-=2;
-		div.style.borderRadius = radius + "px";
-		if (radius == 0)
-		{
-			div.setAttribute("r", 0);
-		}
-		div.setAttribute("radius", radius);
-	}
-	else
-	{
-		radius+=2;
-		div.style.borderRadius = radius + "px";
-		if (radius == 200)
-		{
-			div.setAttribute("r", 1);
-		}
-		div.setAttribute("radius", radius);
-	}
-	
-	setTimeout(animate, 10);
-}
 
 function update_enemy(e)
 {
@@ -58,6 +18,36 @@ function Enemy()
 	body.appendChild(this.o);
 }
 
+var bombs = [];
+
+function create_bomb()
+{
+	bombs[bombs.length] = new Bomb();
+}
+
+function Bomb()
+{
+	this.y = 45;
+	
+	var body = document.getElementsByTagName("body")[0];
+	this.o = document.createElement("div");
+	this.o.className = "bomb";
+	this.o.style.top = this.y + "px";
+	
+	body.appendChild(this.o);
+}
+
+function update_bomb(e)
+{
+	e.y += 5;
+	e.o.style.top= e.y + "px";
+	if (e.y > window.innerHeight - 40)
+	{
+		document.getElementsByTagName("body")[0].removeChild(e.o);
+		return true;
+	}
+}
+
 var enemies = [];
 
 function update()
@@ -67,6 +57,15 @@ function update()
 		update_enemy(enemies[i]);
 	}
 	
+	for(var i = 0; i< bombs.length; ++i)
+	{
+		if(update_bomb(bombs[i]))
+		{
+			bombs.splice(i,1);
+			--i;
+		}
+	}
+	
 	setTimeout(update, 50);
 }
 
@@ -74,6 +73,15 @@ function create_enemies()
 {
 	enemies[enemies.length] = new Enemy();
 	setTimeout(create_enemies, Math.random() * 2000 + 1500);
+}
+
+window.onkeypress =
+function(e)
+{
+	if(e.which == 32 || e.keycode == 32)
+	{
+		create_bomb();
+	}
 }
 
 window.onload = 
